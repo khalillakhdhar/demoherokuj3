c6a3c994c0ef744cece088ee3f778422b17e701b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-30 14:22:18 +0100

    security part1

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.example.demo.controller;
 
+import javax.transaction.Transactional;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Transactional
 public class UserController {
 
 }
